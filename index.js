413f8bf4a3abcf89a6902ad085ecebb202d3bdc7
sdas1 <sdas1@miraclesoft.com>
2023-07-20 23:55:36 +0530

    add-file-system

--- index.js
+++ index.js
@@ -1,4 +1,33 @@
 // const data =require('./math');
-const {sum,minus} =require('./math');
+// const {sum,minus} =require('./math');
 // console.log(data.sum(10,20),data.subtract(20,40));
-console.log(sum(10,20),minus(10,40));
+// console.log(sum(10,20),minus(10,40));
+const data = require('fs');
+// let data2 =data.writeFileSync('./data.text',"hiii my name is subham kumar");
+// let data2=data.writeFile("./name.txt",'hiii my name is karan',((err,res)=>{
+//     if (err) {
+//         console.log('err they file');
+        
+//     }else{
+//         console.log(res);
+//     }
+// }))
+// console.log(data2);
+
+
+// let data2=data.readFileSync('./name.txt','utf-8');
+// console.log(data2);
+// data.readFile('./name.txt','utf-8',((err,result)=>{
+//     if (err) {
+//         console.log('log');
+        
+//     }else{
+//         console.log(result);
+//     }
+// }))
+let data4=data.appendFileSync('./name.txt',`${Date.now()}hii karan this is karan\n `);
+console.log(data4);
+// data.copyFileSync('./name.txt','copy.txt');
+// data.unlinkSync('./copy.txt');
+let data3=data.statSync('./data.text')
+console.log(data3);
