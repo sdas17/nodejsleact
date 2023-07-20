ef77477ffe21fe5c95108534063d33da5dbdcd44
sdas1 <sdas1@miraclesoft.com>
2023-07-20 17:28:47 +0530

    Your commit message here

---
+++ index.js
@@ -0,0 +1,4 @@
+// const data =require('./math');
+const {sum,minus} =require('./math');
+// console.log(data.sum(10,20),data.subtract(20,40));
+console.log(sum(10,20),minus(10,40));
---
+++ math.js
@@ -0,0 +1,16 @@
+// function sum(a,b) {
+//     return a+b
+    
+// }
+// function subtract(a,b) {
+//     return a-b
+    
+// }
+// module.exports="product"
+// module.exports={
+//     sum,
+//     subtract
+// }
+// exports.sum=(a,b)=>a+b;
+// exports.minus=(a,b)=>a-b;
+
